86880a7c410f16cf1291137e0a11f73d9249e046
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-19 20:36:18 -0600

    subir nuevo archivos

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config/static'),
+]
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
