061b9cd28293e0495feb363e54e4361383cf9c79
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-19 21:42:23 -0600

    subir nuevo archivos

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
