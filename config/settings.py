32c38060464fa706400a32a2297c0c6ba1103796
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-19 19:18:19 -0600

    subir nuevo archivos

--- config/settings.py
+++ config/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://localhost@localhost/postgresql',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
